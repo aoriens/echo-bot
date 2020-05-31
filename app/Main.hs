24130d8d67313ecce49ca3a307e3c6e154a4f6e7
Anton Kalinin <aoriens@gmail.com>
2020-05-31 19:40:53 +0300

    Implement /help command

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,39 @@ module Main
   ) where
 
 import Control.Monad
+import Data.Char
+import Data.Text as T
 import Data.Text.IO as TIO
+import System.IO
 
 main :: IO ()
 main = do
   TIO.putStrLn "Welcome to the echo-bot"
-  forever $ TIO.getLine >>= TIO.putStrLn
+  forever $ do
+    TIO.putStr "> "
+    hFlush stdout
+    TIO.putStrLn . responseForRequest . decodeRequest =<< TIO.getLine
+
+data Request
+  = Comment Text
+  | HelpCommand
+
+responseForRequest :: Request -> Text
+responseForRequest (Comment text) = text
+responseForRequest HelpCommand = "This is usage description"
+
+decodeRequest :: Text -> Request
+decodeRequest text
+  | isCommand "/help" = HelpCommand
+  | otherwise = Comment text
+  where
+    isCommand cmd = startsWithWord cmd $ T.stripStart text
+
+startsWithWord :: Text -> Text -> Bool
+startsWithWord word text =
+  case T.stripPrefix word text of
+    Nothing -> False
+    Just rest ->
+      case T.uncons rest of
+        Nothing -> True
+        Just (c, _) -> isSpace c
