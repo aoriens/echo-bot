90ebc1f7168368ba25f604cd4c5f473fe447766e
Anton Kalinin <aoriens@gmail.com>
2020-06-21 20:21:00 +0300

    Add timestamps to logs

--- app/Logger/Impl.hs
+++ app/Logger/Impl.hs
@@ -9,6 +9,7 @@ module Logger.Impl
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Data.Time
 import qualified Logger
 import Prelude hiding (log)
 import qualified System.IO
@@ -25,12 +26,16 @@ new h = Logger.Handle {Logger.hLog = log h}
 log :: Handle -> Logger.Level -> T.Text -> IO ()
 log h level text = do
   when (shouldLog h level) $ do
-    TIO.hPutStrLn (hFileHandle h) $ formatMessage h level text
+    time <- getCurrentTime
+    TIO.hPutStrLn (hFileHandle h) $ formatMessage level time text
     System.IO.hFlush (hFileHandle h)
 
 shouldLog :: Handle -> Logger.Level -> Bool
 shouldLog h level = hMinLevel h <= level
 
-formatMessage :: Handle -> Logger.Level -> T.Text -> T.Text
-formatMessage _ level text =
-  T.justifyLeft 10 ' ' (T.pack (show level) <> ":") <> " " <> text
+formatMessage :: Logger.Level -> UTCTime -> T.Text -> T.Text
+formatMessage level time text =
+  timeString <> " | " <> levelString <> " | " <> text
+  where
+    timeString = T.pack $ formatTime defaultTimeLocale "%F %T.%3q" time
+    levelString = T.justifyLeft 7 ' ' $ T.pack (show level)
